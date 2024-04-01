0cd00602d01e2ec6fc93b8584bdd32d50b794ba3
Anh Nguyen <anhhtvip@gmail.com>
2024-04-01 23:29:37 +0700

    Update env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,11 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       WALLET_CONNECT_PROJECT_ID: process.env.WALLET_CONNECT_PROJECT_ID,
+      OP_BRIDGE_CONTRACT: process.env.OP_BRIDGE_CONTRACT,
+      OP_CHANNEL_ID: process.env.OP_CHANNEL_ID,
+      BASE_BRIDGE_CONTRACT: process.env.BASE_BRIDGE_CONTRACT,
+      BASE_CHANNEL_ID: process.env.BASE_CHANNEL_ID,
+      TIMEOUT: process.env.TIMEOUT,
     },
   },
   vite: {
